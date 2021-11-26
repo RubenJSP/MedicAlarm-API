ecb12f44efb1249835a7b37b5a8ee935410ca5c9
RubenJSP <geneticspartan01@gmail.com>
2021-11-26 09:51:10 -0600

    mail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
 
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseñ') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -37,14 +37,17 @@
                                     </span>
                                 @enderror
                             </div>
+
                         </div>
-
-
+   
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Iniciar sesión') }}
                                 </button>
+                                <a href="{{url('password/reset')}}" class="btn btn-link">
+                                    ¿Olvidaste tu contraseña?
+                                </a>
                             </div>
                         </div>
                     </form>
